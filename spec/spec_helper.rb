80eaedce00fd36d1421cbab0d8396397eecd67b7
Ryan Bigg <radarlistener@gmail.com>
2011-01-28 19:49:37 +1030

    Remove debug crap from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,7 @@ require 'rspec'
 require 'stacked'
 require 'webmock'
 
-puts "REQUIRING EVERYTHING"
-
-Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each { |f| p f; require f }
+Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each { |f| require f }
 
 # Uncomment only for debugging as we only ever want to test against the real API.
 # At least, until it becomes stable.
