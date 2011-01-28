b9a3ec7d4f9e607fd83cf5fa712d4e42fb56422f
Ryan Bigg <radarlistener@gmail.com>
2011-01-28 19:28:57 +1030

    Remove Fakeweb, replace with webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,16 @@ require 'rspec'
 require 'stacked'
 require 'webmock'
 
-Dir["spec/support/**/*.rb"].each { |f| require f }
+puts "REQUIRING EVERYTHING"
+
+Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each { |f| p f; require f }
 
 # Uncomment only for debugging as we only ever want to test against the real API.
 # At least, until it becomes stable.
 # FakeWeb.allow_net_connect = false
 
 RSpec.configure do |c|
+  c.include(WebMock::API)
   c.before(:all) do
     Stacked::Client.configure do |config|
       config.api_key = "FAKE"
